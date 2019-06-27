f400625a0930701caf22e2b6481d70579ace14e6
dedi96 <dedykonteng@gmail.com>
2019-06-27 10:22:58 -0700

    Update script js pemanggil.js

--- script js pemanggil.js
+++ script js pemanggil.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="/2017/08/car-production-fall-hits-uk-industrial.html"}
-if(x=="2"){xxx="/2017/08/tesla-car-battery-production-releases.html"}
-if(x=="3"){xxx="/2017/08/green-lunacy-fossil-fuel-mandates-to.html"}
-if(x=="4"){xxx="/2017/08/us-climate-report-edits-out-highly_28.html"}
-if(x=="5"){xxx="/2017/08/arctic-melt-season-changes-and-arctic.html"}a_to_ve[i].href="https://convert-safelink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="/2018/11/new-therapies-and-benefits-available-in.html"}
+if(x=="2"){xxx="/2018/11/the-best-places-to-invest-in-real.html"}
+if(x=="3"){xxx="/2018/11/mortgage-brokers-reveal-surprise.html"}
+if(x=="4"){xxx="/2018/11/how-to-handle-sharemarket-rollercoaster.html"}
+if(x=="5"){xxx="/2018/11/why-bitcoin-investors-should-feel.html"}a_to_ve[i].href="https://keneono-link.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
