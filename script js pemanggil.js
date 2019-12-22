cb787d04de11f420e73fafb55bf97062aee5298e
dedi96 <dedykonteng@gmail.com>
2019-12-22 16:43:39 +0700

    Update script js pemanggil.js

--- script js pemanggil.js
+++ script js pemanggil.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2018/11/new-therapies-and-benefits-available-in.html"}
 if(x=="2"){xxx="/2018/11/the-best-places-to-invest-in-real.html"}
 if(x=="3"){xxx="/2018/11/mortgage-brokers-reveal-surprise.html"}
 if(x=="4"){xxx="/2018/11/how-to-handle-sharemarket-rollercoaster.html"}
-if(x=="5"){xxx="/2018/11/why-bitcoin-investors-should-feel.html"}a_to_ve[i].href="https://keneono-link.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2018/11/why-bitcoin-investors-should-feel.html"}a_to_ve[i].href="https://loncatin.keneono.site"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
